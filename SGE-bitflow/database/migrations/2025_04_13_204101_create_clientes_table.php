03f9353b00602df3ccfe37ba201e2157f6778fbd
Nicolas Vasquez Suazo <146878865+Yungnico@users.noreply.github.com>
2025-04-13 20:14:23 -0400

    make migration, bug fix

--- SGE-bitflow/database/migrations/2025_04_13_204101_create_clientes_table.php
+++ SGE-bitflow/database/migrations/2025_04_13_204101_create_clientes_table.php
@@ -4,16 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-Schema::create('clientes', function (Blueprint $table) {
-    $table->id();
-    $table->string('razon_social', 100)->unique();
-    $table->string('rut', 12)->unique(); // 12 es suficiente para el formato XX.XXX.XXX-X
-    $table->string('nombre_fantasia', 100)->nullable();
-    $table->string('giro', 100)->nullable();
-    $table->string('direccion', 150)->nullable();
-    $table->string('logo')->nullable(); // Ruta de la imagen
-    $table->timestamps();
-});
 
 return new class extends Migration
 {
@@ -24,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('razon_social', 100)->unique();
+            $table->string('rut', 12)->unique(); // 12 es suficiente para el formato XX.XXX.XXX-X
+            $table->string('nombre_fantasia', 100)->nullable();
+            $table->string('giro', 100)->nullable();
+            $table->string('direccion', 150)->nullable();
+            $table->string('logo')->nullable(); // Ruta de la imagen
             $table->timestamps();
         });
     }
