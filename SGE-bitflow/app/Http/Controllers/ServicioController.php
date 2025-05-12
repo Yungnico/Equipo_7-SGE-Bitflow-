0fa4f0dfe084927590a06926af201c2b31b6580e
ccampos23 <ccamposg@ing.ucsc.cl>
2025-05-11 23:50:39 -0400

    Update ServicioController.php

--- SGE-bitflow/app/Http/Controllers/ServicioController.php
+++ SGE-bitflow/app/Http/Controllers/ServicioController.php
@@ -29,17 +29,13 @@ class ServicioController extends Controller
             'categoria_id' => 'nullable|exists:categorias,id',
         ]);
 
-        // Convertir el precio a tipo float
         $validated['precio'] = (float) $precio;
 
-        // Obtener el nombre de la moneda a partir del ID
         $moneda = Moneda::findOrFail($validated['moneda_id']);
         $validated['moneda'] = $moneda->nombre;
 
-        // Eliminar el campo moneda_id ya que no existe en la tabla servicios
         unset($validated['moneda_id']);
 
-        // Crear el servicio con los datos validados
         Servicio::create($validated);
 
         return redirect()->route('servicios.index')->with('success', 'Servicio registrado exitosamente');
