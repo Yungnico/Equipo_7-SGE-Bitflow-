2d7db43b0a051d8d6305bbd5092790d487b00c2f
Nicolas Vasquez Suazo <146878865+Yungnico@users.noreply.github.com>
2025-05-12 01:24:07 -0400

    Update web.php

--- SGE-bitflow/routes/web.php
+++ SGE-bitflow/routes/web.php
@@ -1,67 +1,64 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Cliente\ClienteController;
-use App\Http\Controllers\Cliente\ContactoClienteController;
-use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\CrudUserController;
+use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\MonedaController;
 use App\Http\Controllers\CotizacionController;
-use App\Models\Cotizacion;
-use Barryvdh\DomPDF\Facade\Pdf;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ClienteExportController;
+use App\Http\Controllers\Cliente\ClienteController;
+use App\Http\Controllers\Cliente\ContactoClienteController;
 
-Route::get('clientes/exportar', [ClienteController::class, 'exportar'])->name('clientes.exportar');
-
-Route::get('/contactos/{contacto}/edit', [ContactoClienteController::class, 'edit'])->name('contactos.edit');
-Route::put('/contactos/{contacto}', [ContactoClienteController::class, 'update'])->name('contactos.update');
-
-Route::resource('clientes.contactos', ContactoClienteController::class)->except(['show']);
-
-
-Route::get('/clientes/buscar', [ClienteController::class, 'buscar'])->name('clientes.buscar');
-Route::get('/clientes/resultados', [ClienteController::class, 'buscar']);
-
-
-
-// Ruta para VER el PDF en el navegador
-Route::post('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.generarPDFobservaciones');
-Route::get('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.generarPDF');
-Route::get('/cotizaciones/{id}/preparar-pdf', [CotizacionController::class, 'prepararPDF'])->name('cotizaciones.prepararPDF')->middleware('auth');
-Route::post('cotizaciones/{id}/enviar', [CotizacionController::class, 'enviarCorreo'])->name('cotizaciones.enviar')->middleware('auth');
-Route::get('/cotizaciones/{id}/Email', [CotizacionController::class, 'prepararEmail'])->name('cotizaciones.prepararEmail');
-Route::get('/cotizaciones/borrador', [CotizacionController::class, 'showBorrador'])->name('cotizaciones.borrador');
-Route::get('/cotizaciones/{id}/edit', [CotizacionController::class, 'edit'])->name('cotizaciones.edit');
-Route::put('/cotizaciones/{id}', [CotizacionController::class, 'update'])->name('cotizaciones.editarestado');
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::get('/cotizaciones/{id}/Email', [CotizacionController::class, 'prepararEmail'])->name('cotizaciones.prepararEmail');
-Route::get('/cotizaciones/create', [CotizacionController::class, 'create'])->name('cotizaciones.create');
-
-Route::middleware('auth')->group(function () {
-    Route::resource('cotizaciones', CotizacionController::class)->except(['edit']);
-    Route::get('/cotizaciones/{id}/info', [CotizacionController::class, 'getCotizacion'])->name('cotizaciones.info');
+// Página de inicio
+Route::get('/', function () {
+    return view('auth.login');
 });
 
-Route::middleware('auth')->group(function () {
+// Rutas de autenticación
+require __DIR__ . '/auth.php';
 
-    // Rutas de Clientes
+// Rutas protegidas por middleware auth
+Route::middleware(['auth'])->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware('verified')->name('dashboard');
+
+    // Perfil
+    Route::get('/perfil', function () {
+        return view('profile.edit', [
+            'user' => Auth::user()
+        ]);
+    })->name('profile.show');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Cambio de contraseña
+    Route::get('/cambiar-password', function () {
+        return view('password.change');
+    })->name('password.change');
+    Route::put('/password', [ProfileController::class, 'updatePassword'])->name('password.update');
+
+    // Registro de usuarios
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+    // Clientes
     Route::resource('clientes', ClienteController::class);
     Route::get('/clientes/{id}/info', [ClienteController::class, 'getCliente'])->name('clientes.info');
+    Route::get('clientes/exportar', [ClienteController::class, 'exportar'])->name('clientes.exportar');
+    Route::get('/clientes/buscar', [ClienteController::class, 'buscar'])->name('clientes.buscar');
+    Route::get('/clientes/resultados', [ClienteController::class, 'buscar']);
 
-    // Rutas de contactos anidados bajo clientes
+    // Contactos de clientes (anidados)
     Route::prefix('clientes/{cliente}')->name('clientes.')->group(function () {
         Route::get('contactos/create', [ContactoClienteController::class, 'create'])->name('contactos.create');
         Route::post('contactos', [ContactoClienteController::class, 'store'])->name('contactos.store');
@@ -69,83 +66,56 @@ Route::middleware('auth')->group(function () {
         Route::get('contactos/info', [ContactoClienteController::class, 'getContactos'])->name('contactos.info');
     });
 
-    // Rutas del perfil
-    Route::prefix('profile')->name('profile.')->group(function () {
-        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
-        Route::patch('/', [ProfileController::class, 'update'])->name('update');
-        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
-    });
+    // Contactos independientes
+    Route::get('/contactos/{contacto}/edit', [ContactoClienteController::class, 'edit'])->name('contactos.edit');
+    Route::put('/contactos/{contacto}', [ContactoClienteController::class, 'update'])->name('contactos.update');
+    Route::resource('clientes.contactos', ContactoClienteController::class)->except(['show']);
 
-    // Dashboard
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->middleware('verified')->name('dashboard');
+    // Cotizaciones
+    Route::get('/cotizaciones/borrador', [CotizacionController::class, 'showBorrador'])->name('cotizaciones.borrador');
+    Route::get('/cotizaciones/create', [CotizacionController::class, 'create'])->name('cotizaciones.create');
+    Route::get('/cotizaciones/{id}/edit', [CotizacionController::class, 'edit'])->name('cotizaciones.edit');
+    Route::put('/cotizaciones/{id}', [CotizacionController::class, 'update'])->name('cotizaciones.editarestado');
+    Route::get('/cotizaciones/{id}/info', [CotizacionController::class, 'getCotizacion'])->name('cotizaciones.info');
 
-});
+    // PDF y correo
+    Route::get('/cotizaciones/{id}/preparar-pdf', [CotizacionController::class, 'prepararPDF'])->name('cotizaciones.prepararPDF');
+    Route::post('/cotizaciones/{id}/enviar', [CotizacionController::class, 'enviarCorreo'])->name('cotizaciones.enviar');
+    Route::get('/cotizaciones/{id}/Email', [CotizacionController::class, 'prepararEmail'])->name('cotizaciones.prepararEmail');
+    // Generar PDF de cotizaciones
+    Route::post('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.generarPDFobservaciones');
+    Route::get('/cotizaciones/{id}/pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.generarPDF');
 
-// Página de bienvenida
+    Route::resource('cotizaciones', CotizacionController::class)->except(['edit']);
 
-Route::get('/', function () {
-    return view('auth.login');
-});
-
-require __DIR__ . '/auth.php';
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-//Cambios en el perfil de la persona logeada
-Route::middleware(['auth'])->group(function () {
-    Route::get('/perfil', function () {
-        return view('profile.edit', [
-            'user' => Auth::user()
-        ]);
-    })->name('profile.show');
-
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-});
-//Cambio de contraseñas
-Route::middleware(['auth'])->group(function () {
-    Route::get('/cambiar-password', function () {
-        return view('password.change');
-    })->name('password.change');
-
-    Route::put('/password', [ProfileController::class, 'updatePassword'])->name('password.update');
-});
-//Registro de usuarios
-Route::middleware(['auth'])->group(function () {
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-});
-
-Route::get('/roles', function () {
-    return view('roles');
-})->middleware(['auth', 'verified']);
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    #SERVICIOS
+    // Servicios
+    Route::get('/servicios', [ServicioController::class, 'index'])->name('servicios.index');
     Route::get('/servicios/crear', [ServicioController::class, 'create'])->name('servicios.create');
     Route::post('/servicios', [ServicioController::class, 'store'])->name('servicios.store');
     Route::get('/servicios/{id}/editar', [ServicioController::class, 'edit'])->name('servicios.edit');
     Route::put('/servicios/{id}', [ServicioController::class, 'update'])->name('servicios.update');
-    Route::get('/servicios', [ServicioController::class, 'index'])->name('servicios.index');
     Route::put('/servicios/{id}/toggle', [ServicioController::class, 'toggleEstado'])->name('servicios.toggleEstado');
     Route::delete('/servicios/{id}', [ServicioController::class, 'destroy'])->name('servicios.destroy');
     Route::get('/servicios/{id}/info', [ServicioController::class, 'getServicio'])->name('servicios.info');
-    #CATEGORIAS
+
+    // Categorías
     Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
     Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
     Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
     Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
     Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
 
+    // Monedas
     Route::resource('monedas', MonedaController::class)->only(['index', 'store', 'update', 'destroy']);
+
+    // Vista y CRUD de usuarios 
+    Route::resource('viewusers', CrudUserController::class)->names('viewusers');
+    Route::get('/viewusers', [CrudUserController::class, 'index'])->name('viewusers.index');
+
 });
 
+// Vista de roles
+Route::get('/roles', function () {
+    return view('roles');
+})->middleware(['auth', 'verified']);
 
-Route::resource('viewusers', CrudUserController::class)->names('viewusers');
-Route::get('/viewusers', [CrudUserController::class, 'index'])->name('viewusers.index');
-require __DIR__ . '/auth.php';
