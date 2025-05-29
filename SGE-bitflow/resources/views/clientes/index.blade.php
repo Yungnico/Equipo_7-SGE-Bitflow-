fe02495e6baa36e937066463b7aa047e11b184e0
ccampos23 <ccamposg@ing.ucsc.cl>
2025-05-29 02:24:11 -0400

    Update index.blade.php

--- SGE-bitflow/resources/views/clientes/index.blade.php
+++ SGE-bitflow/resources/views/clientes/index.blade.php
@@ -1,12 +1,5 @@
 @extends('adminlte::page')
 
-@section('title', 'Clientes')
-
-@section('css')
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
-@stop
-
 @section('content')
 <div class="container-fluid">
     <h1 class="mb-4">Clientes</h1>
@@ -258,18 +251,14 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- DataTables -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
-<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css" />
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         // Inicializar DataTable
         $('#clientes-table').DataTable({
-            responsive: true,
-            autoWidth: false,
             language: {
                 url: '{{ asset("datatables/es-CL.json")}}'
             }
