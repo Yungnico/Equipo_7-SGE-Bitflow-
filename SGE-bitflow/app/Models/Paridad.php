6d88dbb43d957a28f36aad693af7a26c15c55c4b
ccampos23 <ccamposg@ing.ucsc.cl>
2025-05-28 22:16:10 -0400

    debug

--- SGE-bitflow/app/Models/Paridad.php
+++ SGE-bitflow/app/Models/Paridad.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Paridad extends Model
 {
-    
+
     protected $fillable = ['moneda', 'valor', 'fecha'];
 
     protected $dates = ['fecha'];
@@ -17,16 +17,10 @@ class Paridad extends Model
             ->orderBy('fecha', 'desc')
             ->first();
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 
     public function servicios()
     {
         return $this->hasMany(Servicio::class);
     }
->>>>>>> parent of d8c5c8b4 (Merge branch 'Vlillo' of https://github.com/Yungnico/Equipo_7-SGE-Bitflow- into Vlillo)
 }
-=======
-}
->>>>>>> parent of e7e936ae (Merge branch 'Dev' into Vlillo)
