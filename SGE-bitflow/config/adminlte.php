6fb6d20b4c9eda084aaff1bd8ece041311fe5ede
Nicolas Vasquez Suazo <146878865+Yungnico@users.noreply.github.com>
2025-06-14 21:53:23 -0400

    Update adminlte.php

--- SGE-bitflow/config/adminlte.php
+++ SGE-bitflow/config/adminlte.php
@@ -190,13 +190,13 @@ return [
     |
     */
 
-    'classes_body' => '',
+    'classes_body' => 'full-vh',
     'classes_brand' => '',
     'classes_brand_text' => '',
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-primary elevation-4',
+    'classes_sidebar' => 'full-vh sidebar-dark-primary elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-white navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
