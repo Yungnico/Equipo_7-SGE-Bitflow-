175d450951372b0b1b095e92b82a130382080fc6
Neovix33 <112022312+Neovix33@users.noreply.github.com>
2025-06-14 21:13:25 -0400

    responsive tabla borrador

--- SGE-bitflow/resources/views/cotizaciones/borradores.blade.php
+++ SGE-bitflow/resources/views/cotizaciones/borradores.blade.php
@@ -1,12 +1,17 @@
 @extends('adminlte::page')
 @section('title', 'Borradores')
-@section('plugins.Datatables', true)
+@section('css')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+@stop
 
 @section('content')
 <div class="content py-5">
     <div class="card">
         <div class="card-body">
-            <table id="myTable" class="table table-bordered table-striped table-hover">
+            <table id="borrador" class="table table-bordered table-striped table-hover">
                 <thead>
                     <tr>
                         <th>Código Cotización</th>
@@ -44,19 +49,24 @@
 @stop
 
 @section('js')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- Bootstrap 4 JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+
 <script>
     $(document).ready(function () {
-        $('#myTable').DataTable({
-            "language": {
-                responsive: true,
-                autoWidth: true,
+        $('#borrador').DataTable({
+            responsive: true,
+            autoWidth: false,
+            language: {
                 url: '{{ asset("datatables/es-CL.json")}}'
             }
         });
     });
-    
-    $(window).on('resize', function() {
-            table.columns.adjust().responsive.recalc();
-        });
 </script>
 @stop
