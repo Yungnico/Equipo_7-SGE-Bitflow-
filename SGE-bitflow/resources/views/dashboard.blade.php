68dc72ed6f9722cdea47d2e0c9cfae2b546c9d12
Nicolas Vasquez Suazo <146878865+Yungnico@users.noreply.github.com>
2025-06-22 07:38:41 -0400

    Update dashboard.blade.php

--- SGE-bitflow/resources/views/dashboard.blade.php
+++ SGE-bitflow/resources/views/dashboard.blade.php
@@ -5,66 +5,88 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    @can('dashboard')
-        <h1>Futuras Métricas</h1>
-    @endcan
+    
 @stop
 
 @section('content')
-<div class="card">
-    <div class="card-body">
+<div class="row py-5">
+    <!-- Area Chart -->
+    <div class="col-md-6">
+        <x-adminlte-card title="Area Chart" theme="primary" icon="fas fa-chart-area" >
+            <canvas id="areaChart"></canvas>
+        </x-adminlte-card>
+    </div>
 
-        <canvas id="myChart"></canvas>
+    <!-- Line Chart -->
+    <div class="col-md-6">
+        <x-adminlte-card title="Line Chart" theme="primary" icon="fas fa-chart-area" >
+            <canvas id="lineChart"></canvas>
+        </x-adminlte-card>
     </div>
 </div>
-
-@stop
-
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
+@endsection
 
 @section('js')
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    $(document).ready(function() {
-        const ctx = document.getElementById('myChart');
+    const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-            labels: ['1', '2', '3', '4', '5', '6'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
+    const dataCommon = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Blue Data',
+                data: [30, 48, 40, 19, 86, 27, 90],
+                fill: true,
+                borderColor: 'rgba(60,141,188,1)',
+                backgroundColor: 'rgba(60,141,188,0.4)',
+                tension: 0.4
             },
             {
-                label: 'Another Dataset',
-                data: [5, 15, 10, 8, 12, 7],
-                borderWidth: 1
+                label: 'Gray Data',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: true,
+                borderColor: 'rgba(210, 214, 222, 1)',
+                backgroundColor: 'rgba(210, 214, 222, 0.5)',
+                tension: 0.4
             },
             {
-                label: 'Third Dataset',
-                data: [2, 3, 20, 5, 1, 4],
-                borderWidth: 1
+                label: 'Green Data',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: true,
+                borderColor: 'rgba(0,128,0,1)',
+                backgroundColor: 'rgba(0,128,0,0.4)',
+                tension: 0.4
             },
             {
-                label: 'Fourth Dataset',
-                data: [3, 10, 13, 15, 7, 8],
-                borderWidth: 1
-            }]
-            },
-            options: {
-            scales: {
-                y: {
-                beginAtZero: true
-                }
+                label: 'Red Data',
+                data: [18, 48, 77, 9, 100, 27, 40],
+                fill: true,
+                borderColor: 'rgba(255,0,0,1)',
+                backgroundColor: 'rgba(255,0,0,0.4)',
+                tension: 0.4
             }
-            }
-  });
-        console.log(Chart.version)
+        ]
+    };
+
+    new Chart(document.getElementById('areaChart'), {
+        type: 'line',
+        data: JSON.parse(JSON.stringify(dataCommon)), // Clona para modificar
+        options: {
+            responsive: true,
+            plugins: { legend: { display: false } },
+            scales: { y: { beginAtZero: true } }
+        }
+    });
+
+    new Chart(document.getElementById('lineChart'), {
+        type: 'bar',
+        data: JSON.parse(JSON.stringify(dataCommon)),
+        options: {
+            responsive: true,
+            plugins: { legend: { display: false } },
+            scales: { y: { beginAtZero: true } }
+        }
     });
 </script>
-@stop
+@endsection
