d4130ef0d32314b7081761c659f7d9d58a154628
Neovix33 <112022312+Neovix33@users.noreply.github.com>
2025-06-23 00:34:26 -0400

    Errores de laravel

---
+++ SGE-bitflow/resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('errors::minimal')
+
+@section('title', __('404'))
+
+@section('message')
+    {{-- Redirección automática después de 5 segundos al dashboard --}}
+    <meta http-equiv="refresh" content="5;url={{ url('/dashboard') }}">
+    <div class="text-center">
+        <img src="{{ asset('images/logo-amarillo.png') }}" alt="Logo Light" style="width: 150px; height: auto; ">
+    </div>
+    <div class="text-center">
+        <h1 class="text-6xl font-bold mb-2">403</h1>
+        <div class="text-lg text-gray-700 dark:text-gray-300">
+            Página no encontrada.<br>
+            Serás redirigido automáticamente a la pagina principal.
+        </div>
+    </div>
+@endsection
