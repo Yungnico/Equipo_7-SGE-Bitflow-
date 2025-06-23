73192e2c4e427e6ed1732046c52440b827409bc2
Nicolas Vasquez Suazo <146878865+Yungnico@users.noreply.github.com>
2025-06-22 22:23:06 -0400

    Bug costos
    bug fixed

--- SGE-bitflow/app/Http/Controllers/FacturacionController.php
+++ SGE-bitflow/app/Http/Controllers/FacturacionController.php
@@ -236,7 +236,7 @@ class FacturacionController extends Controller
                 'iva' => 'required|numeric',
                 'total' => 'required|numeric',
             ]);
-
+            
             $factura = Facturacion::create([
                 'folio' => $request->folio,
                 'tipo_dte' => $request->tipo_dte,
